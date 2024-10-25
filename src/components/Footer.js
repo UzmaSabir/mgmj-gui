eb729bd9bbf2653802278f28bbd76327d63730d8
UzmaSabir <uzmasdhan@yahoo.com>
2024-10-25 13:33:42 +0200

    add header and footer and simple task page

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// components/Footer.js
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2023 My Company</p>
+        </footer>
+    );
+}
+
+export default Footer;
